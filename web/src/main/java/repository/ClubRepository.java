c20767d82e3a8665777b443a4ac8cc41cd89b73a
PetarBonev1 <163678503+PetarBonev1@users.noreply.github.com>
2025-01-11 17:29:28 +0200

    Delete web directory

--- web/src/main/java/repository/ClubRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import models.Club;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClubRepository extends CrudRepository<Club, String> {
-}
